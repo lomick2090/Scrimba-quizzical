ba31406cf6069c51e2df964262676f632830c24c
minkman <lomick2090@gmail.com>
2023-04-10 18:13:37 -0500

    Ran into bugs with updating answer state

--- quizzical/src/App.jsx
+++ quizzical/src/App.jsx
@@ -5,6 +5,7 @@ import Question from './components/Question'
 export default function App() {
     const [started, setStarted] = React.useState(false)
     const [questions, setQuestions] = React.useState([])
+    const [userAnswers, setUserAnswers] = React.useState([])
 
     function initializeQuestions() {
         fetch('https://opentdb.com/api.php?amount=5')
@@ -14,12 +15,54 @@ export default function App() {
                 data.results.forEach(input => {
                     questionData.push({
                         question: (input.question).replace(/&quot;/g, '"').replace(/&#039;/g, "'").replace(/&eacute;/g, 'e').replace(/&pi;/g, 'pi'),
-                        incorrect_answers: (input.incorrect_answers.map(answer => answer.replace(/&quot;/g, '"').replace(/&#039;/g, "'").replace(/&eacute;/g, 'e').replace(/&pi;/g, 'pi'))),
-                        correct_answer: (input.correct_answer).replace(/&quot;/g, '"').replace(/&#039;/g, "'").replace(/&eacute;/g, 'e').replace(/&pi;/g, 'pi')
+                        incorrect_answers: (input.incorrect_answers.map(answer => answer.replace(/&quot;/g, '"').replace(/&#039;/g, "'").replace(/&eacute;/g, 'e').replace(/&pi;/g, 'pi').replace(/&auml;/g, 'a'))),
+                        correct_answer: (input.correct_answer).replace(/&quot;/g, '"').replace(/&#039;/g, "'").replace(/&eacute;/g, 'e').replace(/&pi;/g, 'pi').replace(/&auml;/g, 'a')
                     }) 
                 })
                 setQuestions(questionData);
+               
+                setUserAnswers(() => {
+                    let newUserAnswers = [];
+                    questionData.map(input => {
+                        newUserAnswers.push({
+                            question: input.question,
+                            answer: ''
+                        })
+                    })
+                    return newUserAnswers
+                })
+                    
+                //     questions.map(input => {
+                //         return {
+                //             question: input.question,
+                //             answer: ''
+                //         }
+                //     })
+                // )
             })
+    }
+
+    function updateAnswer(questionAnswered, newUserAnswer) {
+        console.log(userAnswers)
+        setUserAnswers(prevUserAnswers => {
+            console.log(prevUserAnswers)
+            prevUserAnswers.map(singleUserAnswer => {
+                if (singleUserAnswer.question == questionAnswered) {
+                    return {
+                        ...singleUserAnswer,
+                        answer: newUserAnswer
+                    }
+                } else {
+                    return singleUserAnswer
+                }
+            })
+        })
+
+    }
+
+    function checkAnswers(event) {
+        event.preventDefault()
+        console.log(event)
     }
 
 
@@ -31,6 +74,7 @@ export default function App() {
                 correct_answer={question.correct_answer} 
                 key={question.question}
                 for={question.question}
+                updateAnswer={updateAnswer}
             />
         )
 
@@ -48,7 +92,10 @@ export default function App() {
             
             {
                 (started) ? 
-                    questionElements 
+                    <form>
+                        {questionElements} 
+                        <div className="buttondiv"><button className='submit' onClick={checkAnswers}>check answers</button></div>
+                    </form>
                     :
                     <StartPage 
                         startGame={startGame} 
--- quizzical/src/components/Answer.jsx
+++ quizzical/src/components/Answer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Answer(props) {
     return(
-        <div>
+        <div onClick={() => props.updateAnswer(props.name, props.value)}>
             <input type="radio" id={props.value}  value={props.value} name={props.name} />
             <label className='answer' htmlFor={props.value}>{props.value}</label>
         </div>
