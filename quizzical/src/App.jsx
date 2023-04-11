6844479cb3d74fc8cc8c28ef17fafaea536611fe
minkman <lomick2090@gmail.com>
2023-04-11 12:18:25 -0500

    re-create element creation

--- quizzical/src/App.jsx
+++ quizzical/src/App.jsx
@@ -1,51 +1,82 @@
 import React from 'react'
 import StartPage from './components/StartPage'
 import Question from './components/Question'
+import Answer from './components/Answer'
 
 export default function App() {
     const [started, setStarted] = React.useState(false)
     const [questions, setQuestions] = React.useState([])
     const [userAnswers, setUserAnswers] = React.useState([])
+    const [questionElements, setQuestionElements] = React.useState([])
 
-    function initializeQuestions() {
-        fetch('https://opentdb.com/api.php?amount=5')
-            .then(result => result.json())
-            .then(data => {
-                let questionData = []
-                data.results.forEach(input => {
-                    questionData.push({
-                        question: (input.question).replace(/&quot;/g, '"').replace(/&#039;/g, "'").replace(/&eacute;/g, 'e').replace(/&pi;/g, 'pi'),
-                        incorrect_answers: (input.incorrect_answers.map(answer => answer.replace(/&quot;/g, '"').replace(/&#039;/g, "'").replace(/&eacute;/g, 'e').replace(/&pi;/g, 'pi').replace(/&auml;/g, 'a'))),
-                        correct_answer: (input.correct_answer).replace(/&quot;/g, '"').replace(/&#039;/g, "'").replace(/&eacute;/g, 'e').replace(/&pi;/g, 'pi').replace(/&auml;/g, 'a')
-                    }) 
+    async function initializeQuestions() {
+
+        const result = await fetch('https://opentdb.com/api.php?amount=5')
+        const data = await result.json()
+        
+       
+        //let questionData = []
+        const questionData = await data.results.map(input => {
+            return ({
+                question: (input.question).replace(/&quot;/g, '"').replace(/&#039;/g, "'").replace(/&eacute;/g, 'e').replace(/&pi;/g, 'pi').replace(/&amp;/, '&'),
+                incorrect_answers: (input.incorrect_answers.map(answer => answer.replace(/&quot;/g, '"').replace(/&#039;/g, "'").replace(/&eacute;/g, 'e').replace(/&pi;/g, 'pi').replace(/&auml;/g, 'a').replace(/&amp;/, '&'))),
+                correct_answer: (input.correct_answer).replace(/&quot;/g, '"').replace(/&#039;/g, "'").replace(/&eacute;/g, 'e').replace(/&pi;/g, 'pi').replace(/&auml;/g, 'a')
+            }) 
+        })
+        setQuestions(questionData);
+        
+        
+        setUserAnswers(() => {
+            let newUserAnswers = [];
+            questionData.map(input => {
+                newUserAnswers.push({
+                    question: input.question,
+                    answer: ''
                 })
-                setQuestions(questionData);
-               
-                setUserAnswers(() => {
-                    let newUserAnswers = [];
-                    questionData.map(input => {
-                        newUserAnswers.push({
-                            question: input.question,
-                            answer: ''
-                        })
-                    })
-                    return newUserAnswers
-                })
-                    
-                //     questions.map(input => {
-                //         return {
-                //             question: input.question,
-                //             answer: ''
-                //         }
-                //     })
-                // )
             })
-    }
+            return newUserAnswers
+        })
 
-    function updateAnswer(questionAnswered, newUserAnswer) {
+        setQuestionElements(() => {
+            return questionData.map(question => {
+                let answerElements = []
+    
+                let correctIndex = Math.floor(Math.random() * question.incorrect_answers.length)
+    
+                let answerArrayCopy = question.incorrect_answers.slice()
+    
+                answerArrayCopy.splice(correctIndex, 0, question.correct_answer);
+                
+                
+                for (let j = 0; j < answerArrayCopy.length; j++ ) {
+                    let correct = (j == correctIndex) ? true : false;
+                    answerElements.push(
+                        <Answer 
+                            className='answer' 
+                            value={answerArrayCopy[j]} 
+                            key={j} 
+                            correct={correct} 
+                            name={question.question} 
+                            updateAnswer={updateAnswer} 
+                        />
+                    )
+                }
+
+                return (
+                    <Question 
+                        question={question.question} 
+                        key={question.question}
+                        answerElements={answerElements}
+                    />
+                )
+            })
+        })
+    }
+    
+
+    function updateAnswer(event, questionAnswered, newUserAnswer) {
         console.log(userAnswers)
         setUserAnswers(prevUserAnswers => {
-            console.log(prevUserAnswers)
             prevUserAnswers.map(singleUserAnswer => {
                 if (singleUserAnswer.question == questionAnswered) {
                     return {
@@ -66,19 +97,7 @@ export default function App() {
     }
 
 
-    const questionElements = questions.map(question => {
-        return (
-            <Question 
-                question={question.question} 
-                incorrect_answers={question.incorrect_answers} 
-                correct_answer={question.correct_answer} 
-                key={question.question}
-                for={question.question}
-                updateAnswer={updateAnswer}
-            />
-        )
-
-    })
+    
 
     function startGame() {
         setStarted(true);
--- quizzical/src/components/Question.jsx
+++ quizzical/src/components/Question.jsx
@@ -1,27 +1,11 @@
 import React from 'react';
-import Answer from './Answer'
 
 export default function Question(props) {
-
-    function answerElements() {
-        let correctIndex = Math.floor(Math.random() * props.incorrect_answers.length)
-        let answerArray = props.incorrect_answers
-
-        answerArray.splice(correctIndex, 0, props.correct_answer)
-        let answerElements = []
-        for (let i = 0; i < answerArray.length; i++ ) {
-            let correct = (i == correctIndex) ? true : false;
-            answerElements.push(<Answer className='answer' value={answerArray[i]} key={i} correct={correct} name={props.for} updateAnswer={props.updateAnswer} />)
-        }  
-        
-
-        return answerElements
-    } 
     return (
         <div className='question'>
             <h1>{props.question}</h1>
             <div className="answerdiv">
-                {answerElements()}
+                {props.answerElements}
             </div>
         </div>
     )
